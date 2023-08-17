cc80df36bad4db990e2765950795c8eb0b2c228a
박지은 <31093793+lucykorea414@users.noreply.github.com>
2023-08-17 23:38:13 +0900

    create project

---
+++ pillsafe_searchtest/src/main/java/com/pillsafe/pillsafe_searchtest/PillsafeSearchtestApplication.java
@@ -0,0 +1,13 @@
+package com.pillsafe.pillsafe_searchtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PillsafeSearchtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PillsafeSearchtestApplication.class, args);
+	}
+
+}
---
+++ pillsafe_searchtest/src/test/java/com/pillsafe/pillsafe_searchtest/PillsafeSearchtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pillsafe.pillsafe_searchtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PillsafeSearchtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
